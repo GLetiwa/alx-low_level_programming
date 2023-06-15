07f27cd52db6d84ee67c43c63e8c4c75cf7df89f
GLetiwa <graceletiwa@gmail.com>
2023-06-15 06:12:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,5 +9,10 @@ int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = 59;
-	return (1);
-}	
+
+	/* Use the write system call to print to standard error */
+    if (write(2, str, len) != len)
+        return (1);
+
+    return (1);
+}    
