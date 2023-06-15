02cc4186eceff54856670fbde265027e6992fd29
GLetiwa <graceletiwa@gmail.com>
2023-06-15 11:53:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 
 #include <unistd.h>
-​
-​
+
+
 /**
  * main - Entry point
  *
@@ -11,10 +11,11 @@ int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = 59;
-​
+
 	/* Use the write system call to print to standard error */
 	if (write(2, str, len) != len)
 	return (1);
-​
-​
+
+
 	return (1);
+}
