8e60c08aac1ddf4287e0ed41d3413b30b7051360
GLetiwa <graceletiwa@gmail.com>
2023-06-20 06:29:06 -0700

    10-add.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,13 +16,15 @@ void times_table(void)
 			{
 				_putchar(c + '0');
 			}
+	
 			if (c < 10 && b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
-			} else if (c >= 10)
+			}
+			else if (c >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
