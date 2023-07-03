8793a052893721891c0ae76d4f21376b00cde51b
GLetiwa <graceletiwa@gmail.com>
2023-07-03 14:36:56 +0300

    update

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int g;
+
+	while (*s)
+	{
+		for (g = 0; accept[g]; g++)
+		{
+			if (*s == accept[g])
+			{
+				count++;
+				break;
+			}
+			else if (accept[g + 1] == '\0')
+					return (count);
+		}
+		s++;
+	}
+	return (count);
+}
