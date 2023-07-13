d7f52884f1e5132525b786f9333a2032d795d8dd
GLetiwa <graceletiwa@gmail.com>
2023-07-13 16:14:00 +0300

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,12 +39,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 
-	while (n < len2 && s2[j] != '\0' && i < (len1 + n))
+	while (n < len2 && i < (len1 + n))
 	{
 		s[i++] = s2[j++];
 	}
 
-	while (n >= len2 && s2[j] != '\0' && i < (len1 + len2))
+	while (n >= len2 && i < (len1 + len2))
 	{
 		s[i++] = s2[j++];
 	}
