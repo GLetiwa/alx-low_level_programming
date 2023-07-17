326cab317747f6191e1305aeb786f2104ab0c97a
GLetiwa <graceletiwa@gmail.com>
2023-07-17 22:34:15 +0300

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,76 +9,40 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to new dog(Success), NULL otherwise
+ * Return: pointer to new dog (Success), NULL otherwise
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+	int name_len = strlen(name);
+	int owner_len = strlen(owner);
+
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
+	new_dog->name = malloc((name_len + 1) * sizeof(char));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	strcpy(new_dog->name, name);
 
-	new_dog->owner = strdup(owner);
+	new_dog->owner = malloc((owner_len + 1) * sizeof(char));
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
+	strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
 
 	return (new_dog);
-}
-
-/**
- * _strdup - duplicates a string
- * @s: string to be duplicated
- *
- * Return: pointer to the newly duplicated string
- */
-char *_strdup(char *s)
-{
-	char *dup_s;
-	int len, i;
-
-	if (s == NULL)
-		return (NULL);
-	len = strlen(s);
-	dup_s = malloc((len + 1) * sizeof(char));
-	if (dup_s == NULL)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
-		dup_s[i] = s[i];
-	dup_s = '\0';
-	return (dup_s);
-}
-
-/**
- * _strlen - calculates the length of a string
- * @s: string
- *
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
 }
