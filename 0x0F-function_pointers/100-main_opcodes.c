e8f70dff0116ebc9d7bee534ac39a5372e3f4697
GLetiwa <graceletiwa@gmail.com>
2023-07-19 22:18:10 +0300

    update

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes, i;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	array = (char *)main;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		if (i == num_bytes - 1)
+		{
+			printf("%02hhx\n", array[i]);
+			break;
+		}
+
+		printf("%02hhx", array[i]);
+	}
+	return (0);
+}
