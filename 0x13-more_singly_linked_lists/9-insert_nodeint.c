fcf3361e6254451be3c4d8e4bd61e0b5c2a50e01
GLetiwa <graceletiwa@gmail.com>
2023-08-01 19:27:56 +0300

    trial 3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,40 +10,36 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int index = 0;
+	unsigned int index;
+	listint_t *ptr, *tmp, *new, *c_ptr;
 
-	listint_t *ptr = *head;
-	listint_t *new = malloc(sizeof(listint_t));
+	c_ptr = tmp = *head;
 
-	if (new == NULL)
+	for (index = 0; c_ptr; index++)
+		c_ptr = c_ptr->next;
+
+	if (index < idx)
 		return (NULL);
-
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
-	new->next = NULL;
 
-	if (index == 0)
+	if (idx > 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+		for (index = 0; index < idx; index++)
+		{
+			ptr = tmp;
+			tmp = ptr->next;
+		}
 
-	while (index < idx - 1 && ptr != NULL)
-	{
-		ptr = ptr->next;
-		index++;
-	}
-	if (index == idx - 1 && ptr != NULL)
-	{
-		new->next = ptr->next;
 		ptr->next = new;
-		return (new);
+		new->next = tmp;
 	}
 	else
 	{
-		free(new);
-		return (NULL);
+		*head = new;
+		(*head)->next = tmp;
 	}
-
 	return (new);
 }
