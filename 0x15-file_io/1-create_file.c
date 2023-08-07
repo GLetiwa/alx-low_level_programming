5adfb2c03260ed126d2f32a555df61a4133c4805
GLetiwa <graceletiwa@gmail.com>
2023-08-07 22:25:56 +0300

    trial 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,14 +25,12 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
 	writes = write(fd, text_content, len);
-	if (writes == -1)
-	{
-		close(fd);
+
+	if (fd == -1 || writes == -1)
 		return (-1);
-	}
+
 	close(fd);
+
 	return (1);
 }
